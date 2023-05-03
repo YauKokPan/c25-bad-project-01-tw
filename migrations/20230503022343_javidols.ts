15dd51734ebe1e381f0855f3a4e648d4271c3780
Pan Yau <panpanyau@gmail.com>
2023-05-03 11:01:21 +0800

    Migration

---
+++ migrations/20230503022343_javidols.ts
@@ -0,0 +1,27 @@
+import { Knex } from "knex";
+
+const idolTable = "javidols";
+const galleryTable = "gallery"
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable(idolTable, (table) => {
+    table.increments(); // id
+    table.string("idol_name").notNullable().unique();
+    table.text("idol_info").notNullable();
+    table.string("profile_pic").notNullable();
+    table.timestamps(false, true); // created_at, updated_at
+  });
+  await knex.schema.createTable(galleryTable, (table) => {
+    table.increments(); // id
+    table.integer("idol_id").notNullable();
+    table.foreign("idol_id").references("javidols.id");
+    table.string("idol_image").notNullable;
+    table.timestamps(false, true); // created_at, updated_at
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists(galleryTable);
+    await knex.schema.dropTableIfExists(idolTable);
+}
+
