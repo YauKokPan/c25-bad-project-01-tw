9fe268258b4b2f4eb25883f048e9c87fadca7836
Pan Yau <panpanyau@gmail.com>
2023-05-05 17:43:27 +0800

    update

--- public/index.js
+++ public/index.js
@@ -9,12 +9,12 @@ async function loadIdols() {
     let htmlStr = ``;
     for (const idol of idols) {
       htmlStr += `
-        <div class="idol">
+        <div class="idol" id="idol-${idol.id}">
             <div class="profile-pic">
             <img src="./pictures/javidols-profile-pic/${idol.profile_pic}" width="150px" height="150px">
             </div>
             <div class="idol-name">
-              <a href="#">${idol.idol_name}</a>
+              <a href="./gallery.html">${idol.idol_name}</a>
             </div>
           </div>
       `;
