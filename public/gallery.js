185203a465f415137c929282cc4429fcd6ff9043
Pan Yau <panpanyau@gmail.com>
2023-05-10 11:18:38 +0800

    search update

--- public/gallery.js
+++ public/gallery.js
@@ -105,12 +105,36 @@ async function loadGallery(id) {
     });
   });
 
-  // $(document).ready(function () {
-  //   $(".idol-image").slice(0, displayedImages).fadeIn();
-  //   $(".load-more").click(function(){
-  //     $(".idol-image").slice(0, totalImages).fadeIn();
-  //   })
-  // });
+  const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("[name='query']");
+const searchResults = document.querySelector("#search-results");
+
+searchForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const query = searchInput.value;
+  if (query.trim().length === 0) {
+    searchResults.innerHTML = "<p>No results found.</p>";
+    return;
+  }
+  const response = await fetch("/search?q=" + query, { method: "GET" });
+  const data = await response.json();
+  if (data.length > 0) {
+    searchResults.innerHTML = "";
+    data.forEach((result) => {
+      const resultItem = document.createElement("div");
+      resultItem.innerHTML = `
+        <h3>${result.idol_name}</h3>
+        <a href="./gallery.html?i=${result.id}">
+          <img src="./pictures/javidols-profile-pic/${result.profile_pic}" alt="${result.idol_name}" height="125px">
+        </a>
+        <p>${result.idol_info}</p>
+      `;
+      searchResults.appendChild(resultItem);
+    });
+  } else {
+    searchResults.innerHTML = "<p>No results found.</p>";
+  }
+});
 
   // fancybox
   Fancybox.bind('[data-fancybox="gallery"]', {
