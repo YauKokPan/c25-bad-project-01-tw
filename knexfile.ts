9404db4fdf1c6865c0ebf85021b41c5c4153cdae
skchar <charkai@gmail.com>
2023-05-11 12:36:26 +0800

    add .github for cicd

--- knexfile.ts
+++ knexfile.ts
@@ -36,6 +36,23 @@ const config: { [key: string]: Knex.Config } = {
     },
   },
 
+  test: {
+    client: "postgresql",
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
   production: {
     client: "postgresql",
     connection: {
