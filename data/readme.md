b35010cac3d9583e7549d08d33143bf5fe767934
Pan Yau <panpanyau@gmail.com>
2023-05-11 11:04:10 +0800

    javidol code csv update

--- data/readme.md
+++ data/readme.md
@@ -7,3 +7,8 @@ copy gallery(idol_name,idol_image,idol_id)
 from '/Users/kokpanyau/Documents/c25-bad-project-01-tw/data/javidolsgallery.csv'
 DELIMITER ','
 CSV HEADER;
+
+copy idolcode(idol_name,idol_code,idol_id,title,release_date)
+from '/Users/kokpanyau/Documents/c25-bad-project-01-tw/data/javidolscode.csv'
+DELIMITER ','
+CSV HEADER;
--- migrations/20230503022343_javidols.ts
+++ migrations/20230503022343_javidols.ts
@@ -2,6 +2,7 @@ import { Knex } from "knex";
 
 const idolTable = "javidols";
 const galleryTable = "gallery";
+const codeTable = "idolcode"
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable(idolTable, (table) => {
@@ -20,9 +21,23 @@ export async function up(knex: Knex): Promise<void> {
     table.string("idol_image");
     table.timestamps(false, true); // created_at, updated_at
   });
+  await knex.schema.createTable(codeTable, (table) => {
+    table.increments(); // id
+    table.integer("idol_id");
+    table.foreign("idol_id").references("javidols.id");
+    table.string("idol_name").notNullable();
+    table.string("idol_code");
+    table.text("title");
+    table.string("release_date");
+    table.timestamps(false, true); // created_at, updated_at
+  });
 }
+
+
+
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTableIfExists(galleryTable);
   await knex.schema.dropTableIfExists(idolTable);
+  await knex.schema.dropTableIfExists(codeTable);
 }
